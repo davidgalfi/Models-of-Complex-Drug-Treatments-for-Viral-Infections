0e4de86fcfb2fa0b837197475293398a7047d3bc
GalfiDavid314 <galfidavid314@gmail.com>
2024-03-28 22:27:31 +0100

    # Create DrugDosageInVivo

--- src/main/java/org/example/differential_equation/DrugDosageInVivo.java
+++ src/main/java/org/example/differential_equation/DrugDosageInVivo.java
@@ -1,6 +1,22 @@
 package org.example.differential_equation;
 
-public class DrugDosageInVivo {
+import org.apache.commons.math3.ode.FirstOrderDifferentialEquations;
 
+public class DrugDosageInVivo implements FirstOrderDifferentialEquations {
 
+    public double gamma;
+
+    public DrugDosageInVivo(double gamma) {
+        this.gamma = gamma;
+    }
+
+    @Override
+    public int getDimension() {
+        return 1;
+    }
+
+    @Override
+    public void computeDerivatives(double t, double[] y, double[] yDot) {
+        yDot[0] = -gamma*y[0];
+    }
 }
