3072941d8d4d178e69848ec5cdf46991bb48f39e
GalfiDavid314 <galfidavid314@gmail.com>
2024-04-15 08:21:15 +0200

    # Working

--- src/main/java/org/example/Technical.java
+++ src/main/java/org/example/Technical.java
@@ -8,13 +8,11 @@ public class Technical {
     public static final int Y = 1;
     public double simulationTime;
     public double timeStep;
-    public double interval;
     public int[] dim = new int[2];
 
     public Technical(JSONObject jsonObject) {
         this.simulationTime = (double) jsonObject.get("simulationTime");
         this.timeStep = (double) jsonObject.get("timeStep");
-        this.interval = (double) jsonObject.get("interval");
         this.dim[X] = ((Long) jsonObject.get("xDim")).intValue();
         this.dim[Y] = ((Long) jsonObject.get("yDim")).intValue();
     }
