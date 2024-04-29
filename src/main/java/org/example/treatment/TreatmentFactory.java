4fc12ffec4844e408d5cadb5b863ac7f66077f57
Ferenc Bartha <barfer@math.u-szeged.hu>
2024-04-29 10:32:04 +0200

    just a linebreak

--- src/main/java/org/example/treatment/TreatmentFactory.java
+++ src/main/java/org/example/treatment/TreatmentFactory.java
@@ -9,6 +9,7 @@ public class TreatmentFactory {
     public static Treatment createTreatment(JSONObject treatmentJsonObject, double simulationTime, double timeStep) {
 
         return new Treatment(
+
             DrugFactory.createDrug((JSONObject) treatmentJsonObject.get("Drug")),
             DosageFactory.createDosage((JSONObject) treatmentJsonObject.get("Dosage")),
             simulationTime,
